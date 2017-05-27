0238ca49c1beb3829624bd47547fa242cbe2eaa2
Nolan Davis <nolandavis@Nolans-Air.home>
2017-05-27 14:58:06 -0400

    Fixed radar map and linked to redux store

--- src/components/containers/Radar.js
+++ src/components/containers/Radar.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import withScriptjs from "react-google-maps/lib/async/withScriptjs"
+import FaSpinner from "react-icons/lib/fa/spinner"
+import { connect } from 'react-redux'
 
 class Radar extends Component {
-    constructor(){
-        super()
-        this.state = {
-            selected: null
-        }
-    }
 
     render(){
 
+        const AsyncGettingStartedGoogleMap = withScriptjs(
+            withGoogleMap(
+                props => (
+                  <GoogleMap
+                    defaultZoom={10}
+                    defaultCenter={{ lat: this.props.weather.city.coord.lat, lng: this.props.weather.city.coord.lon }}>
+                  </GoogleMap>
+              )))
+
         return (
-            <h1>Radar map will go here.</h1>
-            // <GoogleMap
-            // //   ref={props.onMapLoad}
-            //   defaultZoom={3}
-            //   defaultCenter={{ lat: -25.363882, lng: 131.044922 }}>
-            // </GoogleMap>
+            <div>
+                <h1>Radar Map</h1>
+                <AsyncGettingStartedGoogleMap
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAGEaUeMMeJv7Sr6WMV4ClussJmDg4Q1K8"
+                    loadingElement={
+                      <div style={{ height: `100%` }}>
+                        <FaSpinner
+                          style={{
+                            display: `block`,
+                            width: `80px`,
+                            height: `80px`,
+                            margin: `150px auto`,
+                            animation: `fa-spin 2s infinite linear`,
+                          }}
+                        />
+                      </div>
+                    }
+                    containerElement={<div style={{ height: `600px`, width: `600px` }} />}
+                    mapElement={<div style={{ height: `600px`, width: `600px` }} />}
+                />
+            </div>
 
         )
     }
 }
 
-export default Radar
+const stateToProps = (state) => {
+    return {
+        weather: state.weather.currentWeather
+    }
+}
+
+export default connect(stateToProps)(Radar)
